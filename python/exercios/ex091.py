e156f5f70b41eea6e7094a3c7cf57c2d52b8c107
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-07-14 21:08:38 -0300

    exercicios

--- python/exercios/ex091.py
+++ python/exercios/ex091.py
@@ -1,3 +1,14 @@
 aluno={}
-aluno.nome=str(input('Nome:'))
-print(aluno)
+aluno['nome']=str(input('Nome:'))
+aluno['media']=float(input(f'média de {aluno["nome"]}:'))
+if aluno['media']<= 5:
+    aluno['situação']="reprovado"
+elif 5<= aluno['media']<=7:
+    aluno['situação']="recuperação"
+else:
+    aluno['situação']="aprovado"
+print('-'*30)
+for k,v in aluno.items():
+    print(f'{k} = {v}')
+
+    
---
+++ python/exercios/ex092.py
@@ -0,0 +1,4 @@
+import random
+
+for c in range(0,6):
+    jogador=random.randint(1,6)
