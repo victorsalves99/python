923d95622269ed9ef73491a2187e4985ed3124ec
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-07-16 14:41:24 -0300

    exerciocio

--- python/exercios/ex092.py
+++ python/exercios/ex092.py
@@ -1,4 +1,29 @@
-import random
 
-for c in range(0,6):
-    jogador=random.randint(1,6)
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogador={
+    'jogador1':randint(1,6),
+    'jogador2':randint(1,6),
+    'jogador3':randint(1,6),
+    'jogador4':randint(1,6),
+}
+print(f'valores sorteados ')
+for k, v in jogador.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(0.5)
+ranking=[]
+ranking=sorted(jogador.items(),key=itemgetter(1), reverse=True)
+print(f'-='*20)
+print(f'  == RANKING DOS JOGADORES == ')
+for a,b in enumerate(ranking):
+    print(f'{a+1} lugar : {b[0]} que tirou {b[1]}')
+    
+
+
+        
+    
+
+
+
+
