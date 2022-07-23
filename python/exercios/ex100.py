957ba26b74bb43ea31623e12cdf2917198f7c07f
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-07-22 22:37:36 -0300

    exercicios

---
+++ python/exercios/ex100.py
@@ -0,0 +1,21 @@
+from time import sleep
+def analise(*num,a=0) :
+    if len(num)==0:
+        num=0
+    print('-='*30)
+    print(f'Analisando valores : ')
+    for c in num:
+        print(f'{c}',end=' ',flush=True)
+        sleep(0)
+    print(f'Foram informados {len(num)} valores. ')
+    print(f'O maior valor informado foi {max(num)}')
+
+
+analise(3,4,5,6,7)
+analise(2,44,24,32)
+analise(3,6,4)
+analise(9,3)
+analise(5,7)
+
+
+
