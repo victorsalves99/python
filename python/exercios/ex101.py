532abde82dc660fb2cc3ea4942ac2b3f068c5891
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-07-25 21:22:13 -0300

    ex resolvido

--- python/exercios/ex101.py
+++ python/exercios/ex101.py
@@ -1,22 +1,26 @@
 from random import randint
+from time import sleep
 pares=[]
 numeros=[]
 def sorteio ():
-    print(f'Sorteado 5 valores da lista',end=' ',flush=True)
+    print(f'Sorteado 5 valores da lista',end=' ')
     for c in range(0,5):
         num=randint(0,9)
-        print(f'{num}',end=' ')
+        sleep(0.8)
+        print(f'{num}',end=' ',flush=True)
         if num%2==0:
             pares.append(num)
             numeros.append(num)
         else:
             numeros.append(num)
             
-    print(f'Pronto')
+    print(f'Pronto!')
 def soma (list):
     print(f'somando os valores pares de {numeros} temos :{sum(list)}')
 
 
-
+print('-='*30)
 sorteio()
+print('-='*30)
 soma(pares)
+print('-='*30)
